0d1d4616cccc74ac16938152335ce79b57cb79ff
jadiasoham <jadiasoham@gmail.com>
2025-07-27 04:32:43 +0000

    feat: uncomment WebSocket debug headers view in chat URLs

--- bodhibot-backend/chat/urls.py
+++ bodhibot-backend/chat/urls.py
@@ -4,5 +4,5 @@ from .views import UserChatView, MessageView, debug_headers_view
 urlpatterns = [
     path('my-chats/', UserChatView.as_view(), name='user-chats'),
     path('messages/', MessageView.as_view(), name= 'chat-messages'),
-    # path('ws/test/', debug_headers_view, name= 'debug_headers_view')
+    path('ws/test/', debug_headers_view, name= 'debug_headers_view')
 ]
