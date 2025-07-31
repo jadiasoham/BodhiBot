97c5e1f995305beffb24eecfed2de61638c03ee3
Soham Jadia <jadiasoham@gmail.com>
2025-07-31 10:40:45 +0530

    feat: add MessageRender component for rendering chat messages with Markdown support

---
+++ bodhibot-frontend/src/components/MessageRender.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import ReactMarkdown from 'react-markdown';
+
+
+const MessageRender = (chatMessage) => {
+  return (
+    <ReactMarkdown
+      remarkPlugins={remarkGfm}
+      rehypePlugins={rehypeRaw}
+    >
+      {chatMessage}
+    </ReactMarkdown>
+  );
+};
+
+export default MessageRender;
