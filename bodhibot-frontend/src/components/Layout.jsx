213dc21569c7d15fd37f64d1206cb9e691c742bf
jadiasoham <jadiasoham@gmail.com>
2025-08-22 09:22:43 +0000

    Update: Layout now contains nav-links

--- bodhibot-frontend/src/components/Layout.jsx
+++ bodhibot-frontend/src/components/Layout.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 
-
-const Layout = ({children}) => {
+const Layout = ({ children }) => {
   return (
-    <>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        minHeight: "100vh", // make full viewport height
+      }}
+    >
       <Header />
-      <main> {children} </main>
+      <main style={{ flexGrow: 1 }}>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 };
 
